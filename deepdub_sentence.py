9344bfc50bd44c02c9f7136cc4960218326cf68a
Anurag Dhadse <hello@adhadse.com>
2022-05-16 06:47:20 +0530

    Update deepdub_sentence.py

--- deepdub_sentence.py
+++ deepdub_sentence.py
@@ -5,20 +5,20 @@ import numpy as np
 
 
 class DeepdubSentence:
+  """Create sentences out of sub file
+  Args:
+    project_name: a project name you might want to give
+    subtitle_path: Path to complete subtitles
+    slice_from (optional): string formatted as `minute_seconds` to set
+      where to clip subs from
+    slice_to (optional): string formatted as `minute_seconds` to set
+      until where subs need to be clipped to.
+     shift (optional): a dictionary with shift values for keys in 
+      (hours, minutes, seconds, milliseconds, ratio) negative values for
+      reverse shift otherwise forward
+    """
   def __init__(self, project_name, subtitle_path,
                **kwargs):
-    """Create sentences out of sub file
-    Parameters:
-      - project_name: a project name you might want to give
-      - subtitle_path`: Path to complete subtitles
-      - slice_from (optional): string formatted as `minute_seconds` to set
-        where to clip subs from
-      - slice_to (optional): string formatted as `minute_seconds` to set
-        until where subs need to be clipped to.
-      - shift (optional): a dictionary with shift values for keys in 
-        (hours, minutes, seconds, milliseconds, ratio) negative values for
-        reverse shift otherwise forward
-    """
     self.SUBS_OUTPUT_PATH = f'./output_dir/{project_name}/subs_gen.srt'
     self.subs = pysrt.open(subtitle_path)
 
